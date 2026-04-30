2474fb5e5f71d5148d5e6f384c14772c282e7a51
Janakar-cloud <janakar.ganesan@gmail.com>
2026-04-30 10:21:09 +0530

    Newslater changes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -61,9 +61,6 @@ export default function Footer({ onNavigate }: FooterProps) {
           {/* Newsletter */}
           <div className="footer-newsletter">
             <h5 className="footer-heading">Engineering Insights</h5>
-            <p className="newsletter-description">
-              Get the latest sustainability updates from our team.
-            </p>
             {/* <form className="newsletter-form" onSubmit={(e) => e.preventDefault()}>
               <input
                 type="email"
